2986a5b484e00d53e2909125bb63d942538eb02e
Sbaoor <2023786106@qq.com>
2021-10-07 11:14:36 +0800

    添加项目文件。

---
+++ KWO/cfg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWO
+{
+    public class cfg
+    {
+        public string bdspath { get; set; }
+        public int wsport { get; set; }
+        public string wskey { get; set; }
+    }
+}
